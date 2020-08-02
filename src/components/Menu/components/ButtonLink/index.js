71ffcdd4b04eba2827e160b37343798daf9baeb8
Fernando Kern <65197222+FernandoDark1@users.noreply.github.com>
2020-08-02 11:38:48 -0300

    Setup inicial do projeto

--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function ButtonLink(props) {
-  // props => { className: "o que alguém passar", href: "/" }
+function ButtonLink({ className, href, children }) {
   return (
-    <a href={props.href} className={props.className}>
-      {props.children}
+    <a className={className} href={href}>
+      {children}
     </a>
   );
 }
 
-export default ButtonLink; 
+ButtonLink.defaultProps = {
+  href: '/',
+  className: '',
+};
+
+ButtonLink.propTypes = {
+  className: PropTypes.string,
+  href: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+export default ButtonLink;
